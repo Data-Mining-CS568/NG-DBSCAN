99baaca806da9258cfb511a4d70d88070910ef9a
vikyath664 <49971328+vikyath664@users.noreply.github.com>
2021-02-04 17:12:29 +0530

    for circular like dataset creation

---
+++ galaxy_type_dataset.py
@@ -0,0 +1,31 @@
+import random
+import math
+
+# radius of the circle
+circle_r = 10
+#starting radius
+start_r = 5
+# center of the circle (x, y)
+circle_x = 5
+circle_y = 7
+
+points=10
+
+circledata=[]
+
+outputfile = open("outputfile.txt","a")
+
+while(points>0):
+    # random angle
+    alpha = 2 * math.pi * random.random()
+    # random radius
+    r = (circle_r-start_r) * math.sqrt(random.random())+start_r
+    # calculating coordinates
+    x = r * math.cos(alpha) + circle_x
+    y = r * math.sin(alpha) + circle_y
+    print("Random point", (x, y))
+    circledata.append((x,y))
+    outputfile.write(str(x)+" "+str(y)+'\n')
+    points=points-1
+print(circledata[1])
+outputfile.close()
