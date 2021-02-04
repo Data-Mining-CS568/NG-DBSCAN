494a385ef80d7a68bf5735b13f63077f24ed7f28
SAMAYV <samay@iitg.ac.in>
2021-02-04 23:14:33 +0530

    dataset

--- NG-DBSCAN Code/clusters_generator.py
+++ NG-DBSCAN Code/clusters_generator.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 l = []
 noise = []
@@ -6,7 +7,7 @@ input_file = open("clusters.txt","r")
 x = input_file.readline()
 a = x.split()
 n = int(a[0])
-#print(n)
+
 for i in range(n):
 	lst = []
 	x = input_file.readline()
@@ -14,41 +15,41 @@ for i in range(n):
 	m = int(a[0])            
 	for j in range(m):
 		x = input_file.readline()
-		#print(x)
 		x = x.split(' ')
 		p = [float(x[0]),float(x[1])]
-		if(x[2]=="noise\n"):
+		if(x[2] == "noise\n"):
 			noise.append(p)
 		else:
 			lst.append(p)
-	l.append(lst)	
+	l.append(lst)
 
-
-import matplotlib.pyplot as plt
-clr=["green","blue","red","orange","yellow","violet","black","pink"]
-i=0
+clr = ["green","blue","red","orange","yellow","violet","black","pink"]
+i = 0
 for cluster in l:
-    x=[]
-    y=[]
+    x = []
+    y = []
     for point in cluster:
         x.append(point[0])
         y.append(point[1])
+
     # plotting points as a scatter plot 
-    plt.scatter(x, y, label= "stars", color= clr[i],marker= "*", s=30)
-    i=(i+1)%6
-    a=[]
-    b=[]
+    plt.scatter(x, y, label = "stars", color = clr[i], marker= "*", s = 30)
+    i = (i + 1) % 6
+    a = []
+    b = []
     for point in noise:
         a.append(point[0])
         b.append(point[1])
-    #print(noise)
-    plt.scatter(a, b, label= "stars", color= "lightblue",marker= "*", s=30)
-  
+    plt.scatter(a, b, label = "stars", color = "lightblue", marker= "*", s = 30)
+
 # x-axis label 
 plt.xlabel('x - axis') 
+
 # frequency label 
 plt.ylabel('y - axis') 
+
 # plot title 
 plt.title('Clusters plot!') 
+
 # function to show the plot 
 plt.show()
