7ecff6c047b2c0ef385533edb7909e30885200d0
SAMAYV <samay@iitg.ac.in>
2021-03-09 19:53:50 +0530

    incremental code begin

---
+++ Inc NG-DBSCAN Code/classes.h
@@ -0,0 +1,68 @@
+class Node {
+	public:
+	int id;
+	string type;
+	Node(int id){
+		this->id = id;
+		this->type = "none";
+	}
+};
+
+class Graph {
+	public:
+	int N;
+	set<int> active;
+	vector<set<int>> edges;
+
+	void add_node(int id){
+		active.insert(id);
+	}
+	Graph(int total_nodes){
+		edges.resize(total_nodes,set<int>());
+		for(int i = 0;i < total_nodes; i++){
+			add_node(i);
+		}
+		this->N = total_nodes;
+	}
+	void remove_node(int id){
+		active.erase(id);
+		edges[id].clear();
+	}
+	void remove_edge(int x, int y){
+		edges[x].erase(y);
+	}
+	void add_edge(int x, int y){
+		edges[x].insert(y);
+	};
+	vector<int> neighbours(int x){
+		vector<int> n;
+		for(auto u : edges[x]){
+			n.push_back(u);
+		}
+		return n;
+	}
+	void clean_graph(){
+		for(int i = 0; i < N; i++){
+			edges[i].clear();
+		}
+	}
+};
+
+class Parameters {
+	public:
+	int k;			
+	int Mmax;	
+	int iter;
+	double epsilon;	 
+	int Minpts; 
+	int threshold;
+
+	Parameters(int k = 20, int Mmax = 20, int iter = 10, double epsilon = 8, int Minpts = 10, int threshold = 100){
+		this->k = k;
+		this->Mmax = Mmax;
+		this->iter = iter;
+		this->epsilon = epsilon;
+		this->Minpts = Minpts;
+		this->threshold = threshold;
+	}
+};
---
+++ Inc NG-DBSCAN Code/code.cpp
@@ -0,0 +1,14 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimization("unroll-loops")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("no-stack-protector")
+
+#include <bits/stdc++.h>
+#include "classes.h"
+using namespace std;
+
+int main()
+{
+	return 0;
+}
