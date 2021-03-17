0c8f42ebf084fab7372f7bd7cc4d88d83364516c
vikyath <49971328+vikyath664@users.noreply.github.com>
2021-03-17 15:53:05 +0530

    Update queries_dataset_generator.py

--- Incremental NG-DBSCAN Code/queries_dataset_generator.py
+++ Incremental NG-DBSCAN Code/queries_dataset_generator.py
@@ -1,3 +1,4 @@
+import random
 from sklearn.datasets import make_moons, make_blobs, make_circles
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
@@ -6,7 +7,6 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-import random
 
 pts = int(input("Enter number of points: \n"))
 dimension = int(input("Since dataset choosen is blob, enter number of dimensions\n"))
@@ -17,13 +17,24 @@ X, y = make_blobs(n_samples = pts, centers = [(1,5),(5,10),(10,5)], n_features =
 # writing in file
 outputfile = open("points.txt","w")
 outputfile.write(str(pts) + " " + str(dimension) + "\n")
-
 for l1 in X:
     for l2 in l1:
-        outputfile.write(str(l2) + " ")
+        s = str(round(l2,3))
+        d=0
+        f=0
+        for i in range(len(s)):
+            if f==1:
+                d=d+1
+            if s[i]=='.':
+                f=1
+        if d==1:
+            s=s+"00"
+        if d==2:
+            s=s+"0"
+        outputfile.write(s + " ")
     outputfile.write("\n")
 
-# graphing
+#graphing
 df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
 colors = {0:'red', 1:'blue', 2:'green'}
 fig, ax = pyplot.subplots()
@@ -32,69 +43,210 @@ for key, group in grouped:
     group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
 pyplot.show()
 
+l = []
+for point in X:
+    l.append([point[0],point[1]])
+#print(l)
+
 # Applying DBSCAN
-l = StandardScaler().fit_transform(X)
-db = DBSCAN(eps=0.1, min_samples=10).fit(l)
+dl = StandardScaler().fit_transform(X)
+db = DBSCAN(eps=0.2, min_samples=10).fit(dl)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 
-# number of clusters
-n_clusters_ = len(set(labels))
 
-list_of_clusters = []
-fake_list = []
+# number of sets
+n_clusters = len(set(labels))
 
-for i in range(n_clusters_):
-    list_of_clusters.append(fake_list)
-
-i = 0
-for label in labels:
-    if label == -1:
-        list_of_clusters[n_clusters_-1].append(X[i])
-    else:
-        list_of_clusters[label].append(X[i])
-    i = i + 1
-
-clr = ["green","blue","red","orange","yellow","violet","pink"]
+#print(len(dic_of_clusters[0]))
+clr=["green","blue","red","orange","yellow","violet","pink"]
 x = []
 y = []
-k = 0
+nx = []
+ny = []
+for k in range(n_clusters):
+    i=0
+    for label in labels:
+        if label==k:
+            x.append([l[i][0]])
+            y.append([l[i][1]])
+        if label==-1:
+            nx.append([l[i][0]])
+            ny.append([l[i][1]])
+        i=i+1
+    if k == (n_clusters-1):
+        plt.scatter(nx, ny, label= "stars", color= "black",marker= ".", s=20)
+    else :
+        plt.scatter(x, y, label= "stars", color= clr[k%7],marker= ".", s=30)
+    x=[]
+    y=[]
+
+plt.xlabel('x - axis') 
+plt.ylabel('y - axis') 
+plt.title('Clusters plot!') 
+plt.show()
+
+
 
 # deleting some points
 num = int(input("Enter number of points to be deleted: \n"))
-x1 = 8
-x2 = 10
-y1 = 4
-y2 = 6
-
-count = 0
-l = []
-
-for i in range(pts):
-    if x1 <= X[i][0] <= x2 and y1 <= X[i][1] <= y2 and count < num:
-        count = count + 1
-        l.append(('D', X[i][0], X[i][1]))
-
-
-# Adding new points
-num = int(input("Enter number of points to be added: \n"))
-x1 = 2
-x2 = 4
-y1 = 6
-y2 = 8
-
-for i in range(num):
-    x = x1 + (x2-x1) * random.random()
-    y = y1 + (y2-y1) * random.random()
-    l.append(('A', x, y))
-
-outputfile = open("queries.txt","w")
-outputfile.write(str(len(l)) + "\n")
-
-random.shuffle(l)
-
-for v in l:
-    outputfile.write(v[0] + " " + str(v[1]) + " " + str(v[2]) + "\n")
-
+x1=8
+x2=10
+y1=4
+y2=6
+outputfile = open("del.txt","w")
+outputfile.write(str(num) + '\n')
+count=0
+for point in l:
+    if x1<=point[0]<=x2 and y1<=point[1]<=y2 and count<num:
+        count=count+1
+        l.remove(point)
+        a = str(round(point[0],3))
+        d=0
+        f=0
+        for i in range(len(a)):
+            if f==1:
+                d=d+1
+            if a[i]=='.':
+                f=1
+        if d==1:
+            a=a+"00"
+        if d==2:
+            a=a+"0"
+        b = str(round(point[1],3))
+        d=0
+        f=0
+        for i in range(len(b)):
+            if f==1:
+                d=d+1
+            if b[i]=='.':
+                f=1
+        if d==1:
+            b=b+"00"
+        if d==2:
+            b=b+"0"
+        outputfile.write(a+' '+b+'\n')
 outputfile.close()
+
+print(len(l))
+
+# Applying DBSCAN
+dl = StandardScaler().fit_transform(l)
+db = DBSCAN(eps=0.2, min_samples=10).fit(dl)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+
+# number of sets
+n_clusters = len(set(labels))
+
+#print(len(dic_of_clusters[0]))
+clr=["green","blue","red","orange","yellow","violet","pink"]
+x = []
+y = []
+nx = []
+ny = []
+for k in range(n_clusters):
+    i=0
+    for label in labels:
+        if label==k:
+            x.append([l[i][0]])
+            y.append([l[i][1]])
+        if label==-1:
+            nx.append([l[i][0]])
+            ny.append([l[i][1]])
+        i=i+1
+    if k == (n_clusters-1):
+        plt.scatter(nx, ny, label= "stars", color= "black",marker= ".", s=20)
+    else :
+        plt.scatter(x, y, label= "stars", color= clr[k%7],marker= ".", s=30)
+    x=[]
+    y=[]
+
+plt.xlabel('x - axis') 
+plt.ylabel('y - axis') 
+plt.title('Clusters plot!') 
+plt.show()
+
+
+#Adding new points
+num = int(input("Enter number of points to be added: \n"))
+x1=2
+x2=5
+y1=6
+y2=9
+outputfile = open("add.txt","w")
+outputfile.write(str(num)+'\n')
+for i in range(num):
+    newx=x1+(x2-x1)*random.random()
+    newy=y1+(y2-y1)*random.random()
+    l.append([newx,newy])
+    a = str(round(newx,3))
+    d=0
+    f=0
+    for i in range(len(a)):
+        if f==1:
+            d=d+1
+        if a[i]=='.':
+            f=1
+    if d==1:
+        a=a+"00"
+    if d==2:
+        a=a+"0"
+    b = str(round(newy,3))
+    d=0
+    f=0
+    for i in range(len(b)):
+        if f==1:
+            d=d+1
+        if b[i]=='.':
+            f=1
+    if d==1:
+        b=b+"00"
+    if d==2:
+        b=b+"0"
+    outputfile.write(a+' '+b+'\n')
+outputfile.close()
+              
+print(len(l))
+
+# Applying DBSCAN
+dl = StandardScaler().fit_transform(l)
+db = DBSCAN(eps=0.2, min_samples=10).fit(dl)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+
+# number of sets
+n_clusters = len(set(labels))
+
+#print(len(dic_of_clusters[0]))
+clr=["green","blue","red","orange","yellow","violet","pink"]
+x = []
+y = []
+nx = []
+ny = []
+for k in range(n_clusters):
+    i=0
+    for label in labels:
+        if label==k:
+            x.append([l[i][0]])
+            y.append([l[i][1]])
+        if label==-1:
+            nx.append([l[i][0]])
+            ny.append([l[i][1]])
+        i=i+1
+    if k == (n_clusters-1):
+        plt.scatter(nx, ny, label= "stars", color= "black",marker= ".", s=20)
+    else :
+        plt.scatter(x, y, label= "stars", color= clr[k%7],marker= ".", s=30)
+    x=[]
+    y=[]
+
+plt.xlabel('x - axis') 
+plt.ylabel('y - axis') 
+plt.title('Clusters plot!') 
+plt.show()
